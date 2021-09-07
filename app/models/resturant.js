5a4b2dffcf22e27ba4cefa3f9925de2024db2c0c
xander <karimi.xander@gmail.com>
2021-09-07 09:01:31 +0430

    resturant model created

---
+++ app/models/resturant.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const schemaComment = mongoose.Schema({
+  user: { type: String, required: true },
+  text: { type: String, required: true },
+  score: { type: Number },
+});
+
+const schemaFood = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  score: { type: Number },
+  price: { type: Number, required: true },
+  picture: { type: String },
+  comments: [schemaComment],
+});
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  addresse: {
+    type: String,
+  },
+  pic: {
+    type: String,
+  },
+  comments: [schemaComment],
+  menu:schemaFood
+});
+
+const model = mongoose.model("resturants", schema);
+module.exports = model;
